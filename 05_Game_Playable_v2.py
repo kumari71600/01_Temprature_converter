c895b2662cc4b449bf88c8556325e0f52c03f72b
kumari71600 <51727297+kumari71600@users.noreply.github.com>
2021-04-08 12:07:08 +1200

    05_game_playable

--- 05_Game_Playable_v2.py
+++ 05_Game_Playable_v2.py
@@ -1,16 +1,18 @@
 from tkinter import *
-
+from functools import partial   # To prevent unwanted windows
 import random
 
-class Start:
-    def __init__self(self, parent):
 
-        # GUI for starting balance and stakes
+class Start:
+    def __init__(self, parent):
+
+        # GUI to get starting balance and stakes
         self.start_frame = Frame(padx=10, pady=10)
         self.start_frame.grid()
 
-        self.push_me_button = Button(text="Push Me", command=self.to_game)
-        self.push_me_button.grid(row=0, pady=10)
+        self.push_me_button = Button(self.start_frame, text="Push me",
+                                     command=self.to_game)
+        self.push_me_button.grid(row=1)
 
     def to_game(self):
 
@@ -18,10 +20,8 @@ class Start:
         starting_balance = 50
         stakes = 2
 
-        Game(self, stakes, starting_balance)
-
-        # hide start up window
         root.withdraw()
+        Game(self, stakes, starting_balance)
 
 
 class Game:
@@ -31,19 +31,26 @@ class Game:
 
         # initialise variables
         self.balance = IntVar()
-        # Set starting balance to amount entered by user  at the start of te game
+        # Set starting balance to amount entered by user at start of the game
         self.balance.set(starting_balance)
 
         # Get value of stakes (use it as a multiplier when calculating winnings
         self.multiplier = IntVar()
         self.multiplier.set(stakes)
 
+        # List for holding statistic
+        self.round_stats_list = []
+
         # GUI Setup
         self.game_box = Toplevel()
+
+        # If users press cross at top, game quits
+        self.game_box.protocol('WM_DELETE_WINDOW', self.to_quit)
+
         self.game_frame = Frame(self.game_box)
         self.game_frame.grid()
 
-        # Heading Rov
+        # Heading Row
         self.heading_label = Label(self.game_frame, text="Play...",
                                    font="Arial 24 bold", padx=10,
                                    pady=10)
@@ -52,57 +59,166 @@ class Game:
         # Instructions Label
         self.instructions_label = Label(self.game_frame, wrap=300, justify=LEFT,
                                         text="Press <enter> or click the 'Open "
-                                             "Boxes' button to reveal the"
+                                             "Boxes' button to reveal the "
                                              "contents of the mystery boxes.",
                                         font="Arial 10", padx=10, pady=10)
         self.instructions_label.grid(row=1)
 
-        # Boxes go here (row=2)
-        box_text = "Arial 16 bold"
-        box_back = "#b9ea96"  # light green
-        box_width = 5
+        # Boxes go here (row 2)
         self.box_frame = Frame(self.game_frame)
         self.box_frame.grid(row=2, pady=10)
 
-        self.prize1_label = Label(self.box_frame, text="?\n", font=box_text,
-                                  bg=box_back, width=box_width, padx=10, pady=10
+        photo = PhotoImage(file="images\question.gif")
+
+        self.prize1_label = Label(self.box_frame, image=photo,
+                                  padx=10, pady=10)
+        self.prize1_label.photo = photo
         self.prize1_label.grid(row=0, column=0)
 
-        self.prize2_label = Label(self.box_frame, text="?\n", font=box_text,
-                                  bg=box_back, width=box_width, padx=10, pady=10
+        self.prize2_label = Label(self.box_frame, image=photo,
+                                  padx=10, pady=10)
+        self.prize2_label.photo = photo
         self.prize2_label.grid(row=0, column=1)
 
-        self.prize3_label = Label(self.box_frame, text="?\n", font=box_text,
-                                  bg=box_back, width=box_width, padx=10, pady=10
+        self.prize3_label = Label(self.box_frame,image=photo,
+                                  padx=10, pady=10)
+        self.prize3_label.photo = photo
         self.prize3_label.grid(row=0, column=2)
 
-        # play button goes here (row=3)
-        self.play_button = Button(self.game_frame, text="open Boxes",
+        # Play button goes here (row 3)
+        self.play_button = Button(self.game_frame, text="Open Boxes",
                                   bg="#FFFF33", font="Arial 15 bold", width=20,
                                   padx=10, pady=10, command=self.reveal_boxes)
         self.play_button.grid(row=3)
 
-        # Balance Label (row 4)
+        # bind button to <enter> (users can push enter to reveal boxes)
 
-        start_text = "Game Cost: ${} \n "" \nHow much " \
-                       "will you win?".format(stakes * 5)
+        self.play_button.focus()
+        self.play_button.bind('<Return>', lambda e: self.reveal_boxes())
 
-        self.balance_label = Label(self.game_frame, font="Arial 12 bold", fg="green"
+        # Balance label (row 4)
+
+        start_text = "Game Cost: ${} \nHow Much " \
+                     "will you win?".format(stakes * 5)
+
+        self.balance_label = Label(self.game_frame, font="Arial 12 bold", fg="blue",
                                    text=start_text, wrap=300,
                                    justify=LEFT)
         self.balance_label.grid(row=4, pady=10)
 
-        # Help and Game Stats button row 5
+        # Help and Game Stats button (row 5)
         self.help_export_frame = Frame(self.game_frame)
         self.help_export_frame.grid(row=5, pady=10)
 
-        self.help_button = Button(self.help_export_frame, text="Help / Rules")
+        self.help_button = Button(self.help_export_frame, text="Help / Rules",
+                                  font="Arial 15 bold",
+                                  bg="#808080", fg="white")
+        self.help_button.grid(row=0, column=0, padx=2)
+
+        self.stats_button = Button(self.help_export_frame, text="Game Stats...",
+                                   font="Arial 15 bold",
+                                   bg="#003366", fg="white")
+        self.stats_button.grid(row=0, column=1, padx=2)
+
+        # Quit Button
+        self.quit_button = Button(self.game_frame, text="Quit", fg="white",
+                                  bg="#660000", font="Arial 15 bold", width=20,
+                                  command=self.to_quit, padx=10, pady=10)
+        self.quit_button.grid(row=6, pady=10)
+
+    def reveal_boxes(self):
+        # retrieve the balance from the initial function...
+        current_balance = self.balance.get()
+        stakes_multiplier = self.multiplier.get()
+
+        round_winnings = 0
+        prizes = []
+        stats_prizes = []
+
+        # Allows photo to change depending on stakes.
+        # lead not in the list as that is always 0
+        copper = ["images\copper_low.gif", "images\copper_med.gif", "images\copper_high.gif"]
+        silver = ["images\silver_low.gif", "images\silver_med.gif", "images\silver_high.gif"]
+        gold = ["images\gold_low.gif", "images\gold_med.gif", "images\gold_high.gif"]
+        for item in range(0, 3):
+            prize_num = random.randint(1,100)
+
+            if 0 < prize_num <= 5:
+                prize = PhotoImage(file=gold[stakes_multiplier-1])
+                prize_list = "gold (${})".format(5 * stakes_multiplier)
+                round_winnings += 5 * stakes_multiplier
+            elif 5 < prize_num <= 25:
+                prize = PhotoImage(file=silver[stakes_multiplier-1])
+                prize_list = "silver (${})".format(2 * stakes_multiplier)
+                round_winnings += 2 * stakes_multiplier
+            elif 25 < prize_num <= 65:
+                prize = PhotoImage(file=copper[stakes_multiplier-1])
+                prize_list = "copper (${})".format(1 * stakes_multiplier)
+                round_winnings += stakes_multiplier
+            else:
+                prize = PhotoImage(file="lead.gif")
+                prize_list = "lead ($0)"
+
+            prizes.append(prize)
+            stats_prizes.append(prize_list)
+
+        photo1 = prizes[0]
+        photo2 = prizes[1]
+        photo3 = prizes[2]
+
+        # Display prizes a edit background...
+        self.prize1_label.config(image=photo1)
+        self.prize1_label.photo = photo1
+        self.prize2_label.config(image=photo2)
+        self.prize2_label.photo = photo2
+        self.prize3_label.config(image=photo3)
+        self.prize3_label.photo = photo3
+
+        # Deduct cost of game
+        current_balance -= 5 * stakes_multiplier
+
+        # Add winnings
+        current_balance += round_winnings
+
+        # Set balance to new balance
+        self.balance.set(current_balance)
+
+        balance_statement = "Game Cost: ${}\nPayback: ${} \n" \
+                            "Current Balance: ${}".format(5 * stakes_multiplier,
+                                                          round_winnings,
+                                                          current_balance)
+
+        # add round results to statistics list
+        round_summary = "[] | [] | {} - Cost: $[] | " \
+                        "Payback: ${} | Current Balance: " \
+                        "${}".format(stats_prizes[0], stats_prizes[1],
+                                     stats_prizes[2],
+                                     5 * stakes_multiplier,round_winnings,
+                                     current_balance)
+        self.round_stats_list.append(round_summary)
+        print(self.round_stats_list)
+
+        # Edit label so user can see their balance
+        self.balance_label.configure(text=balance_statement)
+
+        if current_balance < 5 * stakes_multiplier:
+            self.play_button.config(state=DISABLED)
+            self.game_box.focus()
+            self.play_button.config(text="Game Over")
+
+            balance_statement = "Current Balance: $[]\n" \
+                                "Your balance is too low. You can only quit " \
+                                "or view your stats. Sorry about that.".format(current_balance)
+            self.balance_label.config(fg="#660000", font="Arial 10 bold",
+                                      text=balance_statement)
+
+    def to_quit(self):
+        root.destroy()
 
 
-
-
-
-
-
-
-
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Mystery Box Game")
+    play = Start(root)
+    root.mainloop()
